1493669c9e0ef462cc8f7e7f87c798dd9d9f50bc
Nick Bild <nick.bild@gmail.com>
2021-05-23 20:39:10 -0400

    Add border around screen.

--- rpi_test_vga.py
+++ rpi_test_vga.py
@@ -38,39 +38,6 @@ GPIO.output(interrupt, 0)
 GPIO.output(high_memory_latch, 0)
 
 
-# Draw box representing Arduboy screen area.
-for x in range(36, 165):
-    for y in range(43, 108):
-        i = (y * 200) + x
-
-        v = '{0:015b}'.format(i)
-
-        GPIO.output(a0_7, int(v[7]))
-        GPIO.output(a1_8, int(v[6]))
-        GPIO.output(a2_9, int(v[5]))
-        GPIO.output(a3_10, int(v[4]))
-        GPIO.output(a4_11, int(v[3]))
-        GPIO.output(a5_12, int(v[2]))
-        GPIO.output(a6_13, int(v[1]))
-        GPIO.output(a14, int(v[0]))
-
-        GPIO.output(high_memory_latch, 1)
-        GPIO.output(high_memory_latch, 0)
-
-        GPIO.output(a0_7, int(v[14]))
-        GPIO.output(a1_8, int(v[13]))
-        GPIO.output(a2_9, int(v[12]))
-        GPIO.output(a3_10, int(v[11]))
-        GPIO.output(a4_11, int(v[10]))
-        GPIO.output(a5_12, int(v[9]))
-        GPIO.output(a6_13, int(v[8]))
-
-        GPIO.output(r1, 1)
-
-        GPIO.output(interrupt, 1)
-        GPIO.output(interrupt, 0)
-
-
 # Transform Arduboy x,y coordinates to VGA x,y coordinates.
 def drawPoint(x, y):
     x += 36
@@ -108,3 +75,4 @@ def drawPoint(x, y):
 
 
 drawPoint(0, 0)
+drawPoint(127, 63)
